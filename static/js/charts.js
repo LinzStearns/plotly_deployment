bab94d6abbf1b3b24efd6478ec6b27670aa84fe2
Lindsey Stearns <list4199@gmail.com>
2021-12-11 11:55:52 -0700

    Adding challenge code

--- static/js/charts.js
+++ static/js/charts.js
@@ -27,6 +27,8 @@ function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildMetadata(newSample);
   buildCharts(newSample);
+  buildGaugeChart(newSample);
+  buildBubbleChart(newSample);
   
 }
 
@@ -47,8 +49,9 @@ function buildMetadata(sample) {
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
     Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-    });
+      PANEL.append("h6").text(key.toUpperCase() + ': ' + value); 
+    })
+
 
   });
 }
